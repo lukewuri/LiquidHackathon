c4d4fd547c2425e75738dc8c30800a4dcd5a4630
AndIWasLikeWhaat <ren.selena@gmail.com>
2020-11-07 18:29:59 -0800

    added background, made grid transparent

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,11 @@
 import Button from '@material-ui/core/Button';
 import TabPanels from './TabPanel';
 import React from "react";
+import Background from "./assets/LiquidHacksBackground.png";
 
 function HomePage() {
     return (
-        <div className="HomePage" style={{minHeight: "100vh", backgroundColor: "#d9ddde"}}>
+        <div className="HomePage" style={{minHeight: "100vh", backgroundImage: "url(" + Background + ")", backgroundSize: "cover"}}>
             <TabPanels />
         </div>
     );
